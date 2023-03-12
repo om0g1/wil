7036de68fb4a1357dac0d9e7c26db76cfe092827
om0g1 <omogidavis@gmail.com>
2023-03-12 16:35:16 +0300

    Second Commit

---
+++ project/scripts/progress.gd
@@ -0,0 +1,4 @@
+extends Control
+
+onready var progress_bar = $CenterContainer/VBoxContainer/ProgressBar
+onready var progress_label = $CenterContainer/VBoxContainer/Label
--- scripts/progress.gd
+++
@@ -1,4 +0,0 @@
-extends Control
-
-onready var progress_bar = $CenterContainer/VBoxContainer/ProgressBar
-onready var progress_label = $CenterContainer/VBoxContainer/Label
